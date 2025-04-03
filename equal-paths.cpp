c216aa37e92f33cca607c6d53adf105938661cf7
Yajan Goel <ygoel@usc.edu>
2025-04-03 22:37:29 +0000

    first part done

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -13,6 +13,31 @@ using namespace std;
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if(root == nullptr){
+      return true;
+    }
+    int leafDepth = -1;
+    return checkPath(root, 0, leafDepth);
+}
 
+bool checkPath(Node* root, int depth, int& leafDepth){
+  if(root == nullptr){
+    return true;
+  }
+  if(root -> left == nullptr && root -> right == nullptr){
+    if(leafDepth == -1){
+      leafDepth = depth;
+      return true;
+    }
+    return (depth == leafDepth);
+  }
+  bool leftChild = true, rightChild = true;
+  if(root -> left){
+    leftChild = checkPath(root -> left, depth + 1, leafDepth);
+  }
+  if(root -> right){
+    rightChild = checkPath(root -> right, depth + 1, leafDepth);
+  }
+  return (leftChild && rightChild);
 }
 
